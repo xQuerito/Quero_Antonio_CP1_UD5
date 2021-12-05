b7f965666243a179960cfd5768fbb6cdd88aa244
Antonio Quero Martín <82475311+xQuerito@users.noreply.github.com>
2021-12-05 21:10:57 +0100

    Create juegodedado.php

---
+++ juegodedado.php
@@ -0,0 +1,26 @@
+<?php
+
+//Definición de la clase
+class Dado {
+// Declaración de propiedades
+private $minNumDado = '1';
+private $maxNumDado = '12';
+
+//getterssetters
+public function getMinNumDado()
+{
+	return $this->minNumDado;
+}
+
+public function getMaxNumDado()
+{
+	return $this->maxNumDado;
+}
+
+//Declaración del método
+public function tirarDado(){
+    echo rand ($minNumDado,$maxNumDado);
+}
+}
+
+?>
